d5176cc071c781de6b5705347cc82bf3add63aa9
Shockstick <2152301@talnet.nl>
2021-11-05 10:09:23 +0100

    Eindopdracht

---
+++ Eindopdracht/Eindopdracht.py
@@ -0,0 +1,10 @@
+# Ask the user
+guess = input("Was this lesson helpful? ")
+
+if guess == "Yes":
+    print("Then this was a succes")
+    
+else:
+    print("Then nothing has changed") 
+
+print("Thanks for answering")
--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -2,12 +2,10 @@
 def favorite_videogame(name):
     print("One of my favorite videogames is",name)
 
+
+
+
 favorite_videogame("Team Fortress 2")
 favorite_videogame("Garry's Mod")
 favorite_videogame("Castle Crashers")
 favorite_videogame("Battleblocks theater")
-
-
-
-
-
